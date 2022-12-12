d25a624fdc3ea474322d7ad78d43de9dffa35b7c
Charlie <Charles_Uecker1@baylor.edu>
2022-12-11 19:58:08 -0600

    2nd Commit

--- MainApp/views.py
+++ MainApp/views.py
@@ -16,20 +16,22 @@ def pizzas(request):
 def pizza(request, pizza_id):
     p = Pizza.objects.get(id=pizza_id)
     toppings = Topping.objects.filter(pizza=p)
-    context = {'pizza':p, 'toppings':toppings}
+    addcomment = Comment.objects.filter(pizza=pizza_id)
+    context = {'pizza':p, 'toppings':toppings, 'addcomment':addcomment}
     return render(request, 'MainApp/pizza.html', context)
 
 def comment(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
-
     if request.method != 'POST':
+        form = CommentForm()
+    else:
         form = CommentForm(data=request.POST)
         if form.is_valid():
-            new_comment = form.save(commit=False)
-            new_comment.pizza = pizza
-            new_comment.save()
-    else:
-        form = CommentForm()
-        return redirect('MainApp:pizza',pizza_id=pizza_id)
-    context = {'form':form, 'pizza':pizza}
+            comment=form.save(commit=False)
+            comment.pizza = pizza
+            comment.save()
+
+            return redirect('MainApp:pizza',pizza_id=pizza_id)
+
+    context = {'form':form,'pizza':pizza}
     return render(request, 'MainApp/comment.html', context)
